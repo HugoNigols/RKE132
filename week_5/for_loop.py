2cf06e3c95e85b6b52c574c9f2b6c2ab749ce405
HugoNigols <Hugo.nigols@tktk.ee>
2026-05-03 16:03:38 +0300

    Create for_loop.py

---
+++ week_5/for_loop.py
@@ -0,0 +1,13 @@
+
+# for i in range(1,11):
+#     print(str(i) + ". I will not skateboard in the halls.")
+
+# Kirjutage programm, mis kuvab konsoolis arvud ühest viieni.
+
+""" for i in range (1,6):
+    print(i) """
+
+# Kirjutage programm, mis kuvab konsoolis arvud viieni tagurpidi (st viiest üheni).
+
+for i in range (5, 0, -1):
+    print(i)
